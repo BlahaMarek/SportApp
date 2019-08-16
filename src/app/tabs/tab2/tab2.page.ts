4c3b439facb835c81f16eabbbd11b5ca97cf77c8
Lukáš Obertáš <lukas.obertas@student.tuke.sk>
2019-08-16 22:52:32 +0200

    what a push

--- src/app/tabs/tab2/tab2.page.ts
+++ src/app/tabs/tab2/tab2.page.ts
@@ -11,7 +11,22 @@ export class Tab2Page implements OnInit, AfterContentInit {
   map;
   // @ts-ignore
   @ViewChild('mapElement') mapElement;
+  autocomplete: any;
+  GoogleAutocomplete: any;
+  autocompleteItems: any;
+  zone: any;
+  geocoder: any;
+  markers: any;
+  clearMarkers: any;
+  position: any;
+  marker: any;
   constructor() {
+    this.GoogleAutocomplete = new google.maps.places.AutocompleteService();
+    this.autocomplete = { input: '' };
+    this.autocompleteItems = [];
+    // tslint:disable-next-line:new-parens
+    this.geocoder = new google.maps.Geocoder;
+    this.markers = [];
   }
   ngOnInit(): void {
   }
@@ -24,8 +39,39 @@ export class Tab2Page implements OnInit, AfterContentInit {
   }
 
 
+  updateSearchResults() {
+    if (this.autocomplete.input === '') {
+      this.autocompleteItems = [];
+      return;
+    }
+    this.GoogleAutocomplete.getPlacePredictions({ input: this.autocomplete.input },
+        (predictions, status) => {
+          this.autocompleteItems = [];
+          this.zone.run(() => {
+            predictions.forEach((prediction) => {
+              this.autocompleteItems.push(prediction);
+            });
+          });
+        });
+  }
 
+  selectSearchResult(item) {
+    this.clearMarkers();
+    this.autocompleteItems = [];
 
-
-
+    this.geocoder.geocode({placeId: item.place_id}, (results, status) => {
+      if ( status === 'OK' && results[0]) {
+        const position = {
+          lat: results[0].geometry.location.lat,
+          lng: results[0].geometry.location.lng
+        };
+        const marker = new google.maps.Marker({
+          position: results[0].geometry.location,
+          map: this.map,
+        });
+        this.markers.push(marker);
+        this.map.setCenter(results[0].geometry.location);
+      }
+    });
+  }
 }
